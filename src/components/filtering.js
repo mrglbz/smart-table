a49990fc54b70929d04d37353f4def6fad08beca
Margarita Lobzya <margaritalobzya@gmail.com>
2026-06-13 20:05:08 +0200

    Complete smart table functionality

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,32 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes).forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map((name) => {
+        const option = document.createElement("option");
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        option.value = name;
+        option.textContent = name;
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return option;
+      }),
+    );
+  });
+
+  return (data, state, action) => {
+    if (action && action.name === "clear") {
+      const input = action.parentElement.querySelector("input");
+      const field = action.dataset.field;
+
+      input.value = "";
+      state[field] = "";
     }
+
+    state.total = [state.totalFrom, state.totalTo];
+
+    return data.filter((row) => compare(row, state));
+  };
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,25 +1,47 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js";
 
-/**
- * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
- *
- * @param {Object} settings
- * @param {(action: HTMLButtonElement | undefined) => void} onAction
- * @returns {{container: Node, elements: *, render: render}}
- */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  before.reverse().forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    root.container.prepend(root[subName].container);
+  });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+  after.forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    root.container.append(root[subName].container);
+  });
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+  root.container.addEventListener("change", () => {
+    onAction();
+  });
 
-    return {...root, render};
+  root.container.addEventListener("reset", () => {
+    setTimeout(onAction);
+  });
+
+  root.container.addEventListener("submit", (e) => {
+    e.preventDefault();
+    onAction(e.submitter);
+  });
+
+  const render = (data) => {
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate);
+
+      Object.keys(item).forEach((key) => {
+        if (key in row.elements) {
+          row.elements[key].textContent = item[key];
+        }
+      });
+
+      return row.container;
+    });
+
+    root.elements.rows.replaceChildren(...nextRows);
+  };
+
+  return { ...root, render };
 }
